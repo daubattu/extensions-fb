7f430eca72ee82fcffd339ad897b4f962cef8db9
nguyenhungkhanh <nhk020996@gmail.com>
2018-06-06 22:24:09 +0700

    update get access_token

--- getPagesSource.js
+++ getPagesSource.js
@@ -1,50 +1,60 @@
 function getBodyFB(document_root) {
   var html = '',
-      node = document_root.firstChild;
+    node = document_root.firstChild;
   while (node) {
-      switch (node.nodeType) {
+    switch (node.nodeType) {
       case Node.ELEMENT_NODE:
-          html += node.outerHTML;
-          break;
+        html += node.outerHTML;
+        break;
       case Node.TEXT_NODE:
-          html += node.nodeValue;
-          break;
+        html += node.nodeValue;
+        break;
       case Node.CDATA_SECTION_NODE:
-          html += '<![CDATA[' + node.nodeValue + ']]>';
-          break;
+        html += '<![CDATA[' + node.nodeValue + ']]>';
+        break;
       case Node.COMMENT_NODE:
-          html += '<!--' + node.nodeValue + '-->';
-          break;
+        html += '<!--' + node.nodeValue + '-->';
+        break;
       case Node.DOCUMENT_TYPE_NODE:
-          // (X)HTML documents are identified by public identifiers
-          html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
-          break;
-      }
-      node = node.nextSibling;
-  } 
+        // (X)HTML documents are identified by public identifiers
+        html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
+        break;
+    }
+    node = node.nextSibling;
+  }
   let index = html.indexOf(`["DTSGInitialData",[],{"token":"`),
     lastIndex = index
-  
-  if(index !== -1) {
-    while(html[lastIndex] !== `}`) {
+    access_token = ""
+
+  if(/access_token:"(\w+?)"/.test(html)) {
+    access_token = /access_token:"(\w+?)"/.exec(html)[0].replace(`access_token:"`, "").replace(`"`, "")
+  }
+
+  if (index !== -1) {
+    while (html[lastIndex] !== `}`) {
       lastIndex++;
     }
     html = html.substring(index, lastIndex);
-    
-    let lengthOfFilter = html.length 
-    
-    while(html[lengthOfFilter - 2] !== `"`) {
+
+    let lengthOfFilter = html.length
+
+    while (html[lengthOfFilter - 2] !== `"`) {
       lengthOfFilter--;
     }
     html = `fb_dtsg=${html.substring(lengthOfFilter - 1, html.length - 1).replace(":", "%3")}`
   } else {
     html = "Empty."
   }
-
-  return html
+  return {
+    bodyData: html,
+    access_token
+  }
 }
 
 chrome.runtime.sendMessage({
   action: "getBodyFB",
-  source: getBodyFB(document)
+  source: {
+    bodyData: getBodyFB(document).bodyData,
+    access_token: getBodyFB(document).access_token
+  }
 });
--- popup.js
+++ popup.js
@@ -1,15 +1,15 @@
 chrome.runtime.onMessage.addListener(function (request, sender) {
   if (request.action == "getBodyFB") {
-    message.innerText = request.source;
-  } else if (request.action === "getCookieFB") {
-    console.log("getCookieFB")
-    message.innerText = message.innerText + "message.innerText"
+    message.innerText = request.source.bodyData;
+    access_token.innerText = request.source.access_token
   }
 });
 
 function onWindowLoad() {
 
-  var message = document.querySelector('#message');
+  var message = document.querySelector('#message'),
+    cookieDiv = document.querySelector("#cookie"),
+    access_token = document.querySelector("#access_token")
 
   chrome.tabs.executeScript(null, {
     file: "getPagesSource.js"
@@ -21,11 +21,19 @@ function onWindowLoad() {
   });
 
   chrome.tabs.executeScript(null, { code: `` }, function () {
-    chrome.cookies.getAll({ url: "https://business.facebook.com" }, cookies => console.log(JSON.stringify(cookies)));
-    chrome.runtime.sendMessage({
-      action: "getCookieFB",
-      source: "NHK"
-    })
+    chrome.cookies.getAll({ url: "https://business.facebook.com" }, cookies => {
+      let cookieValue = ""
+      // arrayCookieName = ["sb", "datr", "c_user", "xs", "pl", "fr", "wd", "presence"]
+
+      for(let cookie of cookies) {
+        // console.log(cookie)
+        // cookie = JSON.stringify(cookie)
+        // if(arrayCookieName.includes(cookie.name))
+        cookieValue = cookieValue + `${cookie.name}=${cookie.value}; `
+      }
+      
+      cookieDiv.innerHTML = cookieValue
+    });
   });
 }
 
